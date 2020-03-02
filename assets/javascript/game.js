b69ce5cc9b9ceeffeaa2ceff93954b213e9c6ff6
KAT <katreadwell1@gmail.com>
2020-03-01 17:19:53 -0800

    game is functional, few cosmetic issues

---
+++ assets/javascript/game.js
@@ -0,0 +1,46 @@
+//initializeGame
+//random generate a Target score, with the range of 19-120
+//set global variables: wins = 0, losses = 0, target score, total score = 0
+
+//gameStart
+//function startGame
+
+$(document).ready(function() {
+    var wins = 0;
+    var losses = 0;
+    var targetScore;
+    var score = 0;
+
+    function startNewGame(){
+        score = 0;
+        targetScore = Math.floor(Math.random()*101)+19;
+        $('.pic').each(function(){
+            var crystalNo = Math.floor(Math.random()*11)+1;
+            $(this).attr("crystalNo", crystalNo); 
+        })
+        $("#targetNo").text(targetScore);
+        $("#winsNo").text(wins);
+        $("#lossesNo").text(losses);
+        $("#score").text(score);
+    }
+    startNewGame();
+    $('.pic').on('click', function(){
+        var crystalNo = parseInt($(this).attr("crystalNo"));
+        console.log(typeof crystalNo)
+        score += crystalNo;
+        if(score === targetScore){
+            wins++;
+            startNewGame();
+        }
+        else if(score > targetScore){
+            losses++;
+            startNewGame();
+        }
+        else {
+            $("#score").text(score);
+        }
+
+    })
+
+})
+
