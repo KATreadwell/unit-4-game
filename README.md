2d3d6005e0567ef4baf9e9d6630bc13ebc1a6527
KAT <katreadwell1@gmail.com>
2020-03-07 15:47:18 -0800

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # unit-4-game
+
+In this grippingly-fun mathematical game, you must use your memory and ability to do simple mathematics to choose the correct combination of crystals in order to match the target crystal number.
+
+Creating this game was pretty fun and special thanks to my tutor Jacob Metzinger for introducing me to the .each function!
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,10 @@
 //initializeGame
 //random generate a Target score, with the range of 19-120
 //set global variables: wins = 0, losses = 0, target score, total score = 0
-
-//gameStart
-//function startGame
+//each crystal has random hidden value between 1 - 12.
+//Math.floor(Math.random() * (120 - 19)) + 19; < this is code if you want to use range instead of below
+//gameStart  
+//function startGame  
 
 $(document).ready(function() {
     var wins = 0;
@@ -13,9 +14,12 @@ $(document).ready(function() {
 
     function startNewGame(){
         score = 0;
-        targetScore = Math.floor(Math.random()*101)+19;
+        targetScore = Math.floor(Math.random()*101)+19; 
+
+
         $('.pic').each(function(){
             var crystalNo = Math.floor(Math.random()*11)+1;
+            
             $(this).attr("crystalNo", crystalNo); 
         })
         $("#targetNo").text(targetScore);
